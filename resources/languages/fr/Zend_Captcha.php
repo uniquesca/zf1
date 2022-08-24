977bd3915f434e74576b0d2e84d2ae7dcb046762
George Shestayev <georges@uniques.ca>
2022-08-24 06:17:22 -0700

    Added French translation for Captcha messages.

---
+++ resources/languages/fr/Zend_Captcha.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Translate
+ * @subpackage Ressource
+ * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id:$
+ */
+
+/**
+ * FR-Revision:
+ */
+return array(
+    // Zend_Captcha_ReCaptcha
+    "Missing captcha fields" => "Champs du captcha manquants",
+    "Failed to validate captcha" => "Échec de la validation du captcha",
+    "Captcha value is wrong: %value%" => "La valeur du captcha est incorrecte: %value%",
+
+    // Zend_Captcha_Word
+    "Empty captcha value" => "Valeur du captcha vide",
+    "Captcha ID field is missing" => "Le champ d’identification du captcha est manquant",
+    "Captcha value is wrong" => "La valeur du captcha est incorrecte",
+);
